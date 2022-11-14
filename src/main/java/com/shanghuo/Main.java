bbe7ca232ab141fb0c9d4bc7faf02085922fe369
shang-huo <2062969878@qq.com>
2022-11-14 11:43:03 +0800

    first commit

---
+++ src/main/java/com/shanghuo/Main.java
@@ -0,0 +1,16 @@
+package com.shanghuo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Set<Integer> myset = new HashSet<>();
+        myset.add(1);
+        myset.add(1);
+        System.out.println(myset);
+        Map<String,String> mymap = new HashMap<>();
+    }
+}
